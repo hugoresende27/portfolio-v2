469484b205e835b014826441a7cc86be5411b133
hugoresende27 <hugoresende27@gmail.com>
2023-04-10 20:22:27 +0100

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,39 @@
 <x-guest-layout>
 
-    <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white p-3">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-                <div class="container">
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white px-4 sm:px-20" >
+        @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+            <div class="overflow-hidden rounded-lg bg-gray-700 shadow">
+                <div class="px-4 py-5 sm:p-6">
+                    <!-- Content goes here -->
                     <div>
-                        <div class="flex-shrink-0">
-                            <img class="inline-block h-40 w-30 rounded-full" src="{{asset('my_images/me.jpg')}}" alt="me">
+
+                        <div class="flex">
+                            <div class="mr-4 flex-shrink-0 self-center">
+                                <img class="inline-block w-40 rounded-full" src="{{asset('my_images/me.jpg')}}" alt="me">
+                            </div>
+                            <div>
+                                <h4 class="text-4xl font-bold">About me</h4>
+                                <p class="text-2xl mt-1 text-justify">
+                                    My name is Hugo Resende and I'm a web developer with a passion for working with databases and REST APIs. I am skilled in Laravel, but I also enjoy exploring new PHP and non-PHP frameworks. I am always looking to learn and grow as a developer, and I am excited about the possibilities that new technologies and frameworks can bring.
+                                </p>
+                            </div>
                         </div>
+
 
                         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                             <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
@@ -51,11 +64,13 @@
                         </dl>
                     </div>
                 </div>
+            </div>
 
-        </div>
+
+    </div>
 
 
-    </body>
+
 </x-guest-layout>
 
 
