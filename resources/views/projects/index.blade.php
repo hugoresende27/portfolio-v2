f821369b38f25b59070fcd44d85a2f57e21aa9f6
hugoresende27 <hugoresende27@gmail.com>
2023-04-19 21:37:09 +0100

    feat : weather future 14 days

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,67 @@
+<x-guest-layout>
+
+
+
+    {{--    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white px-4 sm:px-20" >--}}
+    <div class="relative sm:flex sm:justify-center  bg-center dark:bg-dots-lighter dark:bg-gray-900  selection:text-white px-4 sm:px-20" >
+        @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                    {{--                    @if (Route::has('register'))--}}
+                    {{--                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>--}}
+                    {{--                    @endif--}}
+                @endauth
+            </div>
+        @endif
+
+
+
+
+
+        <div class="overflow-hidden ">
+            <div class=" text-center py-16 sm:py-4">
+
+                <x-welcome-first :quote="$quote">
+
+                </x-welcome-first>
+
+                <div class="">
+
+                    {{--                     THIRD BOX--}}
+
+                    <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                        <a href="#">
+                            <li class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
+                                <div class="flex flex-1 flex-col p-8">
+                                    <img class="mx-auto h-32 w-32 flex-shrink-0"
+                                         src="{{asset('my_images/projects/es.jpg')}}" alt="elasticsearch">
+                                </div>
+
+                            </li>
+                        </a>
+
+                        <!-- More people... -->
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+    </div>
+
+
+
+</x-guest-layout>
+
+
