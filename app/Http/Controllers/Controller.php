a756b08ee6369904a04a426877ad49ded42d3006
hugoresende27 <hugoresende27@gmail.com>
2023-04-24 02:35:50 +0100

    feat : swagger docs start

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *    title="Your super  ApplicationAPI",
+ *    version="1.0.0",
+ * )
+ */
 
 class Controller extends BaseController
 {
--- app/Models/Elasticdemo.php
+++ app/Models/Elasticdemo.php
@@ -4,6 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+/**
+ * @OA\Schema(
+ *     @OA\Xml(name="Elasticdemo"),
+ *     @OA\Property(property="id", type="integer", readOnly=true, example=33),
+ *     @OA\Property(property="name", type="string", description="Name", example="Alanna Hill Jr.s"),
+ *     @OA\Property(property="street", type="string", description="Address", example="82258 Larue Walk Suite 407"),
+ *     @OA\Property(property="number", type="string", maxLength=10, example="2953"),
+ *     @OA\Property(property="code", type="string", maxLength=10, example="01819"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", example="2023-04-23T23:23:17.000000Z"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", example="2023-04-23T23:26:11.000000Z"),
+ * )
+ *
+ * Class Elasticdemo
+ */
 
 class Elasticdemo extends Model
 {
