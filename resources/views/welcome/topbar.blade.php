1d7216e915ef3d9f6f96e97347d9f9a8c9dc1a05
hugoresende27 <hugoresende27@gmail.com>
2023-05-14 20:58:22 +0100

    hotfix : mobile top bar

--- resources/views/welcome/topbar.blade.php
+++ resources/views/welcome/topbar.blade.php
@@ -11,7 +11,7 @@
     <button type="button" class="rounded-full px-4 py-2.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-gray-900">CV</button>
 </a>
 <a href="{{route('contact-me')}}">
-    <button type="button" class="rounded-full px-4 py-2.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-gray-900">Contact me</button>
+    <button type="button" class="rounded-full px-4 py-2.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-gray-900 mt-5">Contact me</button>
 </a>
 
 <hr class="mt-5 mb-5">
